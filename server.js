c54097644f166a7b67ec86291d889ef8efb00aef
Amparo Olivares <amparolivares18@gmail.com>
2025-01-29 17:53:46 +0100

    primeros endpoints actualizados y añadiendo los endpoints de los filtros

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ let zonas = [
 ];
 
 let establecimientos = [
-    { id: '9', name: 'Bar Pepe', zonasId: 3 },
-    { id: '8', name: 'Bar 2', zonasId: 2 },
-    { id: '10', name: ' Bar 3', zonasId: 3 }
+    { id: '9', name: 'Bar Pepe', zonasId: 3, dietaId: 16, preciosId: 23 },
+    { id: '8', name: 'Bar 2', zonasId: 2, dietaId: 14, preciosId: 17 },
+    { id: '10', name: ' Bar 3', zonasId: 3, dietaId: 15, preciosId: 17 }
 ]
 
 let filtros = [
@@ -28,13 +28,19 @@ let dieta = [
 
 let precios = [
     { id: '17', precioMin: '1', precioMax: '10'},
-    { id: '18', precioMax: '25' },
+    { id: '18', precioMin: '10', precioMax: '25' },
+    { id: '23', precioMin: '25', precioMax: '50' }
+]
+
+let alergenos = [
+    { id: '19', name: 'frutos secos'},
+    { id: '20', name: 'marisco'},
+    { id: '21', name: 'huevo'},
+    { id: '22', name: 'gluten'}
 ]
 
 app.use(express.json());
 app.use(morgan('tiny'));
-
-
 
 // API
 
@@ -42,7 +48,7 @@ app.get('/zonas', (req, res) => {
     res.json(zonas);
 });
 
-app.get('/zonas/:id/establecimientos', (req,res) => {
+app.get('/zonas/:id/establecimientos', (req,res) => { // llama a los establecimientos de esos zonas
     const zonasId = req.params.id; //2
     const resultado = establecimientos.filter((establecimiento) => establecimiento.zonasId == zonasId);
     res.json(resultado);
@@ -50,21 +56,26 @@ app.get('/zonas/:id/establecimientos', (req,res) => {
 
 //endpoint se une al anterior
 app.get('/zonas/:id/establecimientos', (req, res) => {
-    const {precioMin, precioMax , dieta, alergenos} = req.query;
-
-    // buscar todos los establecimientos de la zona que ha elegido
-    const zonasId = req.params.id; //2
-    const resultado = establecimientos.filter((establecimiento) => establecimiento.zonasId == zonasId);
+    const {precioId, dietaId, alergenosId} = req.query;
+    const zonasId = req.params.id; //LLAMAMOS  A LOS ESTABLECIMIENTOS CON ESA :ID
+    const establecimientosPorZonas = establecimientos.filter((establecimiento) => establecimiento.zonasId == zonasId); //LLAMAMOS  A LOS ESTABLECIMIENTOS QUE CONTENGAN ESA ID
 
 
-    if(precioMin && precioMax) {
-        //filtros precio: algo parecido a la linea 57 pero con Pmax i Pmin
+
+    if(precioId) {
+        establecimiento.zonasId == precioId;
     }
 
-    //hacer lo muismo con dieta y alergenos
+    if(dietaId) {
+        establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) => establecimiento.dieta?.includes(dietaId) ); // Asumimos que 'dieta' es un array en cada establecimiento.
 
+    }
 
+    if(alergenosId) {
+        establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) =>  return alergenosId.some(alergeno) => establecimiento.alergenos?.includes(alergeno)); // Excluimos establecimientos que contengan alguno de los alérgenos
+    }
 
+    res.json(establecimientosPorZonas);
 
 });
 
