eeba9e254aa1249e13ed32af2265868b28428a35
yodralopez <yodralopez@gmail.com>
2025-02-07 13:06:19 +0000

    resolve conflicts and format code

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -2,85 +2,89 @@
 // MongoDB Playground
 // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
 
-const database = 'TURISMO_DB';
-const collection = 'ZONAS';
-
 // The current database to use.
 use('TURISMO_DB');
 
 // Create a new collection.
 db.createCollection('ESTABLECIMIENTOS');
+db.createCollection('ZONAS');
 
 
 db.ZONAS.insertMany([
-   { nombre: "Centro",
+    {
+        nombre: "Centro",
+    },
+    {
+        nombre: "Casco Antiguo",
+    },
+    {
+        nombre: "Puerto",
+    },
+]);
 
-   },
+let ZONASId = db.ZONAS.findOne({nombre: "Centro"})._id;
 
-   {nombre: "Casco Antiguo",
-
-   },
-
-   { nombre: "Puerto",
-
-   },
-
-    ]); 
-
-let ZONASId = db.ZONAS.findOne({ nombre: "Centro" })._id;
-
-    db.ESTABLECIMIENTOS.insertMany([
-    { name: "Bar Pepe",
+db.ESTABLECIMIENTOS.insertMany([
+    {
+        name: "Bar Pepe",
         direccion: "Casa miswebos 23",
         descripcion: "aowoaowoaowooawooaow que rico",
         precio: "10-15",
         dieta: "Flexitariana",
         paginaWeb: "www.barpepe.com",
-        zonaId: ZONASId },
-
-    { name: "Bar Manolo",
+        zonaId: ZONASId
+    },
+    {
+        name: "Bar Manolo",
         direccion: "Casa caracul 34",
         descripcion: "sjahsdjkdhjkdahjk masmalo",
         precio: "40-50",
         dieta: "Vegetariana",
         paginaWeb: "www.barmanolo.com",
-        zonaId: ZONASId },
-
-    { name: "Bar Paqui",
+        zonaId: ZONASId
+    },
+    {
+        name: "Bar Paqui",
         direccion: "Casa llepapiles 45",
         descripcion: "el peor masmalo",
         precio: "20-25",
         dieta: "Vegana",
         paginaWeb: "www.barpaqui.com",
-        zonaId: ZONASId }
+        zonaId: ZONASId
+    }
+]);
 
-    ]);
+ZONASId = db.ZONAS.findOne({nombre: "Casco Antiguo"})._id;
 
-ZONASId = db.ZONAS.findOne({ nombre: "Casco Antiguo" })._id;
-
-    db.ESTABLECIMIENTOS.insertMany([
-    { name: "Bar Xikilicuatre",
+db.ESTABLECIMIENTOS.insertMany([
+    {
+        name: "Bar Xikilicuatre",
         direccion: "Casa miswebos 23",
         descripcion: "aowoaowoaowooawooaow que rico",
         precio: "10-15",
         dieta: "Flexitariana",
         paginaWeb: "www.barxikilicuatre.com",
-        zonaId: ZONASId },
+        zonaId: ZONASId
+    },
 
-    { name: "Bar MikuMikuUiu",
+    {
+        name: "Bar MikuMikuUiu",
         direccion: "Casa caracul 34",
         descripcion: "sjahsdjkdhjkdahjk masmalo",
         precio: "40-50",
         dieta: "Vegetariana",
         paginaWeb: "www.barmiku.com",
-        zonaId: ZONASId },
+        zonaId: ZONASId
+    },
 
-    { name: "Bar Madri",
+    {
+        name: "Bar Madri",
         direccion: "Casa llepapiles 45",
         descripcion: "el peor masmalo",
         precio: "20-25",
         dieta: "Vegana",
         paginaWeb: "www.barmadri.com",
-        zonaId: ZONASId }
+        zonaId: ZONASId
+    }
 
-    ]); 
+]);
--- server.js
+++ server.js
@@ -1,88 +1,82 @@
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
-<<<<<<< HEAD
-import { MongoClient } from 'mongodb';
-import cors from 'cors';
-
-=======
 import { MongoClient, ObjectId } from 'mongodb';
-//Cors
-const app = express();
->>>>>>> bb8fa579e81f57d41afcdb80e7a7959781256a14
-const mongoUrl = 'mongodb://localhost:27017';
-const app = express();
 
-
+const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
+const mongoUrl = 'mongodb://localhost:27017';
 let db;
 
-MongoClient.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true}) 
-.then(client => {
-    console.log('Conectado a MongoDB');
-    db = client.db('TURISMO_DB'); // Asignar la base de datos
-    }) .catch(err => { console.error('Error al conectar a MongoDB:', err);
-    
-    }); 
-    
+MongoClient.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(client => {
+        console.log('Conectado a MongoDB');
+        db = client.db('TURISMO_DB'); // Asignar la base de datos
+    }).catch(err => {
+    console.error('Error al conectar a MongoDB:', err);
+});
+
 
 // API
-
-<<<<<<< HEAD
-
-
 app.get('/zonas', async (req, res) => {
 
-    try { 
+    try {
         console.log("conectando a mongodb");
         if (!db) {
-            return res.status(500).json({ error: 'La conexión a la base de datos no está lista' });
-        } 
+            return res.status(500).json({error: 'La conexión a la base de datos no está lista'});
+        }
         const data = await db.collection('ZONAS').find().toArray(); // Buscamos en la colección correcta
         console.log('✅ Datos obtenidos:', data);
         res.json(data);
     } catch (err) {
         console.error('❌ Error al obtener los datos:', err);
-        res.status(500).json({ error: err.message });
+        res.status(500).json({error: err.message});
     }
-    });
+});
 
-=======
-app.get('/zonas', async (req, res) => { 
-    try { // Realiza una búsqueda en la colección "ESTABLECIMIENTOS" para obtener los establecimientos que tengan el "zona_id" igual a zonasId 
-   const resultado = await db.collection('ZONAS').find.toArray();
-       res.json(resultado); } catch (error) { 
-       console.error('Error al obtener zonas:', error);
-       res.status(500).json({ error: 'Hubo un problema al obtener las zonas' }); } });
->>>>>>> bb8fa579e81f57d41afcdb80e7a7959781256a14
+app.get('/zonas', async (req, res) => {
+    try { // Realiza una búsqueda en la colección "ESTABLECIMIENTOS" para obtener los establecimientos que tengan el "zona_id" igual a zonasId
+        const resultado = await db.collection('ZONAS').find.toArray();
+        res.json(resultado);
+    } catch (error) {
+        console.error('Error al obtener zonas:', error);
+        res.status(500).json({error: 'Hubo un problema al obtener las zonas'});
+    }
+});
 
+// TODO Recuarden borrar este endpoint y usar el de abajo ;)
 app.get('/zonas/:id/establecimientos', async (req, res) => {
-     const zonasId = req.params.id; // Zona ID que viene en la URL (por ejemplo, '60c72b2f9e1d8c3efcb15d10')
-     try { // Realiza una búsqueda en la colección "ESTABLECIMIENTOS" para obtener los establecimientos que tengan el "zona_id" igual a zonasId 
-    const resultado = await db.collection('ESTABLECIMIENTOS').find({ zona_id: new ObjectId(ZONASId) }).toArray();
-      // Enviar el resultado de la consulta a MongoDB como respuesta JSON
-        res.json(resultado); } catch (error) { 
+    const zonasId = req.params.id; // Zona ID que viene en la URL (por ejemplo, '60c72b2f9e1d8c3efcb15d10')
+    try { // Realiza una búsqueda en la colección "ESTABLECIMIENTOS" para obtener los establecimientos que tengan el "zona_id" igual a zonasId
+        const resultado = await db.collection('ESTABLECIMIENTOS').find({zona_id: new ObjectId(ZONASId)}).toArray();
+        // Enviar el resultado de la consulta a MongoDB como respuesta JSON
+        res.json(resultado);
+    } catch (error) {
         console.error('Error al obtener establecimientos:', error);
-        res.status(500).json({ error: 'Hubo un problema al obtener los establecimientos' }); } });
+        res.status(500).json({error: 'Hubo un problema al obtener los establecimientos'});
+    }
+});
 
 //endpoint se une al anterior
 app.get('/zonas/:id/establecimientos', async (req, res) => {
     const {precioId, dietaId, alergenosId} = req.query;
     const zonasId = req.params.id; //LLAMAMOS  A LOS ESTABLECIMIENTOS CON ESA :ID
-    const establecimientosPorZonas = await db.collection('ESTABLECIMIENTOS').find({ zona_id: new ObjectId(zonasId) }).toArray();
+    let establecimientosPorZonas = await db.collection('ESTABLECIMIENTOS').find({zona_id: new ObjectId(zonasId)}).toArray();
 
-    if(precioId) {
-        establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) => establecimiento.precioId==precioId);   // establecimiento.zonasId == precioId;
+    if (precioId) {
+        establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) => establecimiento.precioId == precioId);   // establecimiento.zonasId == precioId;
     }
 
-    if(dietaId) {
-        establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) => establecimiento.dietaId==dietaId); // Asumimos que 'dieta' es un array en cada establecimiento.
+    if (dietaId) {
+        establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) => establecimiento.dietaId == dietaId); // Asumimos que 'dieta' es un array en cada establecimiento.
 
     }
 
-    if(alergenosId) {
+    if (alergenosId) {
         establecimientosPorZonas = establecimientosPorZonas.filter((establecimiento) => establecimiento.alergenosId == alergenosId); // Excluimos establecimientos que contengan alguno de los alérgenos
     }
 
@@ -92,7 +86,7 @@ app.get('/zonas/:id/establecimientos', async (req, res) => {
 
 app.get('/', (req, res) => {
     res.send('¡Servidor funcionando correctamente!');
-  });
+});
 
 app.listen(3000, () => {
     console.log('Ready on port 3000!');
