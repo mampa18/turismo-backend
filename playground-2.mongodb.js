38d2e06d3326cac3ccd10c04616a3bf7a1901b18
luzamora <luciagargon7@gmail.com>
2025-02-11 11:46:01 +0100

    base de datos actualizada

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -29,31 +29,40 @@ let zonasId = db.zonas.findOne({nombre: "Centro"})._id;
 // Insertar establecimientos en 'Centro'
 db.establecimientos.insertMany([
     {
-        name: "Bar Pepe",
-        direccion: "Casa miswebos 23",
-        descripcion: "aowoaowoaowooawooaow que rico",
-        precio: "10-15",
+        name: "El Damasol",
+        direccion: "Calle Castaños, 25, 03001 Alicante",
+        descripcion: "Un restaurante especializado en cocina alicantina con toques caseros. Destacan su arroz meloso de pescado y sus tapas elaboradas con productos frescos del mar.",
+        precio: "€€€",
         dieta: "Flexitariana",
-        paginaWeb: "www.barpepe.com",
-        zonaId: zonasId
+        telefono: "965 20 76 99",
+        paginaWeb: "www.eldamasol.com",
+        zonaId: zonasId,
+        imagen: "https://estaticos.elmundo.es/assets/multimedia/imagenes/2016/12/30/14831205166665.jpg"
     },
     {
-        name: "Bar Manolo",
-        direccion: "Casa caracul 34",
-        descripcion: "sjahsdjkdhjkdahjk masmalo",
-        precio: "40-50",
+        name: "Bodhigreen Vegetariano",
+        direccion: "Calle San Fernando, 46 Bajo(cafe Belice), 03001 Alicante España",
+        descripcion: "Restaurante amplio con suelo de madera, que sirve zumos de frutas y platos vegetarianos y sin gluten.",
+        precio: "€€",
         dieta: "Vegetariana",
-        paginaWeb: "www.barmanolo.com",
-        zonaId: zonasId
+        telefono: "965 21 31 44",
+        paginaWeb: "http://www.bodhigreen.es/",
+        zonaId: zonasId,
+        imagen:"https://www.ociomagazine.es/uploads/2016/01/resized/900_bodhigreen-restaurantes-ociomagazine-alicante-8.jpg",
+        alergeno:"Gluten" 
     },
+
     {
-        name: "Bar Paqui",
-        direccion: "Casa llepapiles 45",
-        descripcion: "el peor masmalo",
-        precio: "20-25",
+        name: "El buen comer",
+        direccion: "C. Mayor, 8, 03002 Alicante",
+        descripcion: "Tapas, pescado, marisco y arroces de la cocina española en un restaurante informal con detalles de madera y terraza.",
+        precio: "€",
         dieta: "Vegana",
-        paginaWeb: "www.barpaqui.com",
-        zonaId: zonasId
+        telefono: "965 21 35 41",
+        paginaWeb: "https://www.elbuencomer.es/",
+        zonaId: zonasId,
+        imagen:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/60/9f/c6/salon-planta-baja-reformado.jpg?w=900&h=-1&s=1",
+        alergeno: "Mariscos",
     }
 ]);
 
@@ -63,31 +72,28 @@ zonasId = db.zonas.findOne({nombre: "Casco Antiguo"})._id;
 // Insertar establecimientos en 'Casco Antiguo'
 db.establecimientos.insertMany([
     {
-        name: "Bar Xikilicuatre",
-        direccion: "Casa miswebos 23",
-        descripcion: "aowoaowoaowooawooaow que rico",
-        precio: "10-15",
-        dieta: "Flexitariana",
-        paginaWeb: "www.barxikilicuatre.com",
-        zonaId: zonasId
+        name: "Nou Manolín",
+        direccion: "Calle Mayor, 12, 03002 Alicante",
+        descripcion: "Fundado en 1971, este bar icónico ofrece productos de alta calidad como gambas rojas, arroz y jamón ibérico. Su barra es considerada una de las mejores del mundo.",
+        precio: "€€€",
+        dieta: "Mariscos y arroces",
+        telefono: "965 20 03 68",
+        paginaWeb: "www.noumanolin.com",
+        zonaId: zonasId,
+        imagen: "https://grupogastronou.com/wp-content/uploads/10.jpg",
+        alergeno: "Mariscos"
     },
     {
-        name: "Bar MikuMikuUiu",
-        direccion: "Casa caracul 34",
-        descripcion: "sjahsdjkdhjkdahjk masmalo",
-        precio: "40-50",
-        dieta: "Vegetariana",
-        paginaWeb: "www.barmiku.com",
-        zonaId: zonasId
-    },
-    {
-        name: "Bar Madri",
-        direccion: "Casa llepapiles 45",
-        descripcion: "el peor masmalo",
-        precio: "20-25",
-        dieta: "Vegana",
-        paginaWeb: "www.barmadri.com",
-        zonaId: zonasId
+        name: "La Ereta",
+        direccion: "Parque de la Ereta, s/n, 03001 Alicante",
+        descripcion: "Con impresionantes vistas de la ciudad, La Ereta combina innovación y tradición en su cocina. Su chef, Dani Frías, ofrece una reinterpretación moderna de los sabores locales.",
+        precio: "€€€",
+        dieta: "Gastronomía local innovadora",
+        telefono: "965 14 32 50",
+        paginaWeb: "www.laereta.es",
+        zonaId: zonasId,
+        imagen: "https://res.cloudinary.com/tf-lab/image/upload/restaurant/0e244f51-ef99-4240-b6b2-6e922bf71acc/545c7a59-9681-46a6-8ec0-8f4735536151.jpg",
+        alergeno: "Varía según el plato"
     }
 ]);
 
@@ -96,31 +102,40 @@ zonasId = db.zonas.findOne({nombre: "Puerto"})._id;
 // Insertar establecimientos en 'Puerto'
 db.establecimientos.insertMany([
     {
-        name: "Bar Xikilicuatre",
-        direccion: "Casa miswebos 23",
-        descripcion: "aowoaowoaowooawooaow que rico",
-        precio: "10-15",
-        dieta: "Flexitariana",
-        paginaWeb: "www.barxikilicuatre.com",
-        zonaId: zonasId
+        name: "La Sastrería",
+        direccion: "Av. Conde de Vallellano, 8, 03001 Alicante",
+        descripcion: "Un restaurante especializado en cocina alicantina con toques caseros. Destacan su arroz meloso de pescado y sus tapas elaboradas con productos frescos del mar.",
+        precio: "€€",
+        dieta: "Pescado y mariscos",
+        telefono: "965 20 31 74",
+        paginaWeb: "www.lasastreriaalicante.com",
+        zonaId: zonasId,
+        imagen: "https://media-cdn.tripadvisor.com/media/photo-s/1d/f5/a9/01/mesa-la-sastreria.jpg",
+        alergeno: "Pescado"
     },
     {
-        name: "Bar MikuMikuUiu",
-        direccion: "Casa caracul 34",
-        descripcion: "sjahsdjkdhjkdahjk masmalo",
-        precio: "40-50",
-        dieta: "Vegetariana",
-        paginaWeb: "www.barmiku.com",
-        zonaId: zonasId
+        name: "Falafelmanía",
+        direccion: "C/ de Perú, 13 Local 2E, 03008 Alicante España",
+        descripcion: "Auténtica cocina casera, exclusiva Vegetariana y Vegana, hecha con los exquisitos sabores del Medio Oriente y servida en deliciosos Menús de Degustación",
+        precio: "€",
+        dieta: "Vegano",
+        telefono: "966 08 07 39",
+        paginaWeb: "https://www.facebook.com/falafelmania",
+        zonaId: zonasId,
+        imagen: "https://media-cdn.tripadvisor.com/media/photo-s/1d/f5/a9/01/mesa-la-sastreria.jpg",
+        alergeno: "Pescado"
     },
     {
-        name: "Bar Madri",
-        direccion: "Casa llepapiles 45",
-        descripcion: "el peor masmalo",
-        precio: "20-25",
-        dieta: "Vegana",
-        paginaWeb: "www.barmadri.com",
-        zonaId: zonasId
+        name: "César Anca",
+        direccion: "Explanada de España, 3, 03002 Alicante",
+        descripcion: "Con un enfoque en tapas modernas, César Anca reinterpreta clásicos españoles con creatividad, como sus huevos rotos con queso ahumado y romero.",
+        precio: "€€",
+        dieta: "Tapas creativas",
+        telefono: "965 14 94 65",
+        paginaWeb: "www.cesaranca.com",
+        zonaId: zonasId,
+        imagen: "https://www.cesaranca.com/wp-content/uploads/2024/07/CESARANCA_LABARRA-14.jpg",
+        alergeno: "Huevos"
     }
 ]);
 
@@ -130,6 +145,6 @@ let zonaBusqueda = "Centro"; // Cambia esto según la zona que quieras consultar
 let zonaId = db.zonas.findOne({nombre: zonaBusqueda})._id;
 
 // Consulta para obtener los establecimientos de esa zona
-let establecimientos = db.ESTABLECIMIENTOS.find({ zonaId: zonaId }).toArray();
+let establecimientos = db.establecimientos.find({ zonaId: zonaId }).toArray();
 
 // Mostrar los establecimientos encontrados
